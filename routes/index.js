9f11489630f5989e2b19ff29cbe266130f733b6d
Bhavitha <you@bpenaka@my.centennial.ca.com>
2022-10-01 19:56:35 -0400

    last commit

---
+++ routes/index.js
@@ -0,0 +1,35 @@
+
+var express = require('express');
+var router = express.Router();
+
+/* GET Home page. */
+router.get('/', function(req, res, next) {
+  res.render('homePg', { title: 'Home'});
+});
+
+/* GET Home page. */
+router.get('/home', function(req, res, next) {
+  res.render('homePg', { title: 'Home'});
+});
+
+/* GET About Us page. */
+router.get('/about', function(req, res, next) {
+  res.render('about', { title: 'About'});
+});
+
+/* GET Projects page. */
+router.get('/projects', function(req, res, next) {
+  res.render('projects', { title: 'Projects'});
+});
+
+/* GET Services page. */
+router.get('/services', function(req, res, next) {
+  res.render('service', { title: 'Services'});
+});
+
+/* GET Contact Us page. */
+router.get('/contact', function(req, res, next) {
+  res.render('contact', { title: 'Contact'});
+});
+
+module.exports = router;
