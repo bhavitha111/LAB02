a31da5e0abfd48784e3b2193ebc6cf44142c897d
Bhavitha <you@bpenaka@my.centennial.ca.com>
2022-10-22 22:28:30 -0400

    updated

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,7 @@
 module.exports = 
 {
-    "URI": "mongodb://localhost/business_contacts"
+    //"URI": "mongodb://localhost/business_contacts",
+    "URI":"mongodb+srv://businesscontacts:dgc9zJq08TaCLnHR@cluster0.llitd3t.mongodb.net/?retryWrites=true&w=majority"
+
+    
 }
